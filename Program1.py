484f322dce537637d343de5258b84d838215f28e
Tejas P Naik <161220624+tejaspnaik@users.noreply.github.com>
2025-11-16 22:16:57 +0530

    Update Program1.py

--- Program1.py
+++ Program1.py
@@ -5,7 +5,7 @@ from Crypto.Util.Padding import pad, unpad
 
 key = b'8bytekey'
 cipher = DES.new(key, DES.MODE_ECB)
-
+#u can replace DES.block_size with 8 cause size is 8bytes
 ct = cipher.encrypt(pad(b"hellosymmetricDES", DES.block_size))
 pt = unpad(cipher.decrypt(ct), DES.block_size)
 
